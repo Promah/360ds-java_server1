4eb5e1aa93eab034c8c9486dca163e053bc48974
vovaspk <carlostevez228@gmail.com>
2019-07-10 21:20:57 +0300

    Initial commit

---
+++ src/test/java/com/onseo/courses/ds/controllers/UserControllerTest.java
@@ -0,0 +1,24 @@
+package com.onseo.courses.ds.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserControllerTest {
+
+    @Test
+    public void authUser() {
+
+    }
+
+    @Test
+    public void userlist() {
+
+    }
+
+    @Test
+    public void user() {
+
+    }
+
+}
