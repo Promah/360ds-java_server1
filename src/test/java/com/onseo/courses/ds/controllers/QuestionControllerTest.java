aedc160f8801d2e113f44b67627e2be183ffe025
Sergii Kravchuk <serhiikravhuk@gmail.com>
2019-07-11 11:29:07 +0300

    Add QuestionController and tmp class TestClassResponse

---
+++ src/test/java/com/onseo/courses/ds/controllers/QuestionControllerTest.java
@@ -0,0 +1,28 @@
+package com.onseo.courses.ds.controllers;
+
+import org.junit.Test;
+
+public class QuestionControllerTest {
+
+    @Test
+    public void getQuestionsTest(){
+
+    }
+
+    @Test
+    public void getAnswersTest(){
+
+    }
+
+    @Test
+    public void getSummaryTest(){
+
+    }
+
+    @Test
+    public void getResultsTest(){
+
+    }
+
+
+}
