f7031cd4ba9407a22c4d9ca3456e615f27522810
Sergii Kravchuk <serhiikravhuk@gmail.com>
2019-07-11 11:29:07 +0300

    Add QuestionController and tmp class TestClassResponse

---
+++ src/main/java/com/onseo/courses/ds/controllers/TestClassResponse.java
@@ -0,0 +1,15 @@
+package com.onseo.courses.ds.controllers;
+
+//class for tmp controller testing
+public class TestClassResponse {
+
+    private final String response;
+
+    public TestClassResponse(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+}
