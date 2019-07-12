4debab2557557c4cfaf1605325775fd807d03ca9
Sergii Kravchuk <serhiikravhuk@gmail.com>
2019-07-12 18:27:24 +0300

    Slitted controllers to interface and implementation

--- src/test/java/com/onseo/courses/ds/controllers/QuestionControllerTest.java
+++ src/test/java/com/onseo/courses/ds/controllers/QuestionControllerTest.java
@@ -1,6 +1,23 @@
 package com.onseo.courses.ds.controllers;
 
+import com.onseo.courses.ds.Application;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 
 public class QuestionControllerTest {
 
