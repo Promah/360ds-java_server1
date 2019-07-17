fcece0a5259ef075121ed3c66c004b8cf3b8adff
deprilch <rokkymike@gmail.com>
2019-07-17 17:21:05 +0300

    QuizSummary default

---
+++ src/test/java/com/onseo/courses/ds/quiz/quizSummary/QuizSummaryTest.java
@@ -0,0 +1,56 @@
+package com.onseo.courses.ds.quiz.quizSummary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuizSummaryTest {
+
+    @Test
+    public void setQuizAssignmentId() {
+    }
+
+    @Test
+    public void setQuizId() {
+    }
+
+    @Test
+    public void setQuizPersonAnswered() {
+    }
+
+    @Test
+    public void setQuizPersonTotal() {
+    }
+
+    @Test
+    public void setQuizStatus() {
+    }
+
+    @Test
+    public void setQuizTitle() {
+    }
+
+    @Test
+    public void getQuizPersonAnswered() {
+    }
+
+    @Test
+    public void getQuizPersonTotal() {
+    }
+
+    @Test
+    public void getQuizStatus() {
+    }
+
+    @Test
+    public void getQuizAssignmentId() {
+    }
+
+    @Test
+    public void getQuizId() {
+    }
+
+    @Test
+    public void getQuizTitle() {
+    }
+}
