46b53290c4ca3ed80f83d3de2aaaeaa70a0b2a0d
anckort <anckort11@gmail.com>
2019-07-17 17:38:22 +0300

    added connection to database, modified class "User"

---
+++ src/main/java/com/onseo/courses/ds/service/UserService.java
@@ -0,0 +1,12 @@
+package com.onseo.courses.ds.service;
+
+import com.onseo.courses.ds.entytyuser.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    public List<User> getAllUsers();
+    public void createUser(User user);
+    public Optional<User> getUserById(String id);
+}
