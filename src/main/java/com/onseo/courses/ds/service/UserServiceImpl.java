ff96b7690f99c4708f13e746402c0cee93a9983e
Ola <shiki.lo.ukr@gmail.com>
2019-07-21 13:49:25 +0300

    Changes for user and new mocks

--- src/main/java/com/onseo/courses/ds/service/UserServiceImpl.java
+++ src/main/java/com/onseo/courses/ds/service/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package com.onseo.courses.ds.service;
 
-import com.onseo.courses.ds.entytyuser.User;
+import com.onseo.courses.ds.entityuser.User;
 import com.onseo.courses.ds.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
