143df17841a60e2bbe9bfadca1bb5e87641c3da1
Dmitro Svyrydovych <svyrydovich@gmail.com>
2019-07-23 09:22:12 +0300

    change logger

--- src/main/java/com/onseo/courses/ds/logger/Logging.java
+++ src/main/java/com/onseo/courses/ds/logger/Logging.java
@@ -4,7 +4,7 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class Logging {
-    public static final Logger logger = LogManager.getRootLogger();
+    private static final Logger logger = LogManager.getRootLogger();
 
     public static Logger getLogger() {
         return logger;
