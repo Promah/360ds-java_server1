7220442a49ef5854a689b824b9f03b2fa935c80e
deprilch <rokkymike@gmail.com>
2019-07-26 15:00:20 +0300

    quizSummary mocks and controller finished controller creates list of quizSummary entities

---
+++ src/main/java/com/onseo/courses/ds/controllers/QuizSummaryController.java
@@ -0,0 +1,49 @@
+package com.onseo.courses.ds.controllers;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onseo.courses.ds.quiz.Quiz;
+import com.onseo.courses.ds.quiz.quizSummary.QuizSummary;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizSummaryController {
+
+    public List<QuizSummary> getQuizSummaries() {
+        List<QuizSummary> quizSummaries = null;
+
+        try {
+            quizSummaries = getQuizListFromFile();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return quizSummaries;
+    }
+
+    private List<QuizSummary> getQuizListFromFile() throws Exception{
+        Object objUser = new JSONParser().parse(new FileReader(getClass().getClassLoader().getResource("mocks/quiz_summary_mock").getFile()));
+        JSONArray array = (JSONArray) objUser;
+        List<QuizSummary> list = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++){
+            list.add(mapFromJson(((JSONObject)array.get(i)).toJSONString(), QuizSummary.class));
+        }
+        return list;
+    }
+
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
+
+}
