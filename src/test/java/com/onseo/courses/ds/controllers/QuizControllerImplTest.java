14a6fe0e57a23de44db2e90531b8b4c648efb71f
Kulpa1 <kulpekin20@gmail.com>
2019-07-30 21:34:09 +0300

    created endpoint and done changes in json files

--- src/test/java/com/onseo/courses/ds/controllers/QuizControllerImplTest.java
+++ src/test/java/com/onseo/courses/ds/controllers/QuizControllerImplTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -37,5 +38,17 @@ public class QuizControllerImplTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void postQuizSubmit() throws Exception {
+        String url = "/api/quiz/submit/1";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(url)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        int resultStatus = mvcResult.getResponse().getStatus();
+        assertEquals(200,resultStatus);
     }
 }
